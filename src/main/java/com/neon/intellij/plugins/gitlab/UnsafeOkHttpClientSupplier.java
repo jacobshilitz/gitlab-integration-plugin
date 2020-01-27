398975293f268bd5bcb1ea425630dcf222bfea71
David Suárez <dsuarez@telcomunity.com>
2020-01-27 11:20:43 +0100

    Fix trust manager access on jdk > 9

--- src/main/java/com/neon/intellij/plugins/gitlab/UnsafeOkHttpClientSupplier.java
+++ src/main/java/com/neon/intellij/plugins/gitlab/UnsafeOkHttpClientSupplier.java
@@ -4,7 +4,9 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 
 import javax.net.ssl.*;
+import java.security.KeyStore;
 import java.security.cert.CertificateException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -43,8 +45,18 @@ public class UnsafeOkHttpClientSupplier implements Supplier< OkHttpClient > {
             // Create an ssl socket factory with our all-trusting manager
             final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
 
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+            TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init((KeyStore) null);
+            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+            if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+                throw new IllegalStateException("Unexpected default trust managers:"
+                                                + Arrays.toString(trustManagers));
+            }
+            X509TrustManager trustManager = (X509TrustManager) trustManagers[0];
+
             OkHttpClient.Builder builder = new OkHttpClient.Builder();
-            builder.sslSocketFactory(sslSocketFactory);
+            builder.sslSocketFactory(sslSocketFactory, trustManager);
             builder.hostnameVerifier(new HostnameVerifier() {
                 @Override
                 public boolean verify(String hostname, SSLSession session) {
